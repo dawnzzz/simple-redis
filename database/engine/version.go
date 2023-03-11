55f46cf67d9d3e9c9bf508975a1878a69da5de65
dawnzzz <635441876@qq.com>
2023-03-11 17:45:18 +0800

    添加单机watch机制

---
+++ database/engine/version.go
@@ -0,0 +1,28 @@
+package engine
+
+// addVersion 为指定的keys版本号+1
+func (db *DB) addVersion(keys ...string) {
+	for _, key := range keys {
+		versionCode := db.GetVersion(key)
+		db.versionMap.Put(key, versionCode+1)
+	}
+}
+
+// GetVersion 获取指定key值的版本号
+func (db *DB) GetVersion(key string) uint32 {
+	entity, ok := db.versionMap.Get(key)
+	if !ok {
+		return 0
+	}
+	return entity.(uint32)
+}
+
+func (db *DB) checkVersionChanged(watching map[string]uint32) bool {
+	for key, ver := range watching {
+		currentVersion := db.GetVersion(key)
+		if ver != currentVersion {
+			return true
+		}
+	}
+	return false
+}
