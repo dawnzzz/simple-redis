d9bb007959a7849fe20feaf3e07e4758218c20dc
dawnzzz <635441876@qq.com>
2023-04-02 21:38:55 +0800

    补全EntityToReply

--- lib/utils/marshal.go
+++ lib/utils/marshal.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"github.com/dawnzzz/simple-redis/datastruct/dict"
+	List "github.com/dawnzzz/simple-redis/datastruct/list"
+	"github.com/dawnzzz/simple-redis/datastruct/set"
+	"github.com/dawnzzz/simple-redis/datastruct/sortedset"
 	"github.com/dawnzzz/simple-redis/interface/database"
 	"github.com/dawnzzz/simple-redis/redis/protocol/reply"
 	"strconv"
@@ -9,6 +13,10 @@ import (
 
 var (
 	setCmd       = []byte("SET")
+	zAddCmd      = []byte("ZADD")
+	hSetCmd      = []byte("HSET")
+	sAddCmd      = []byte("SADD")
+	rPushCmd     = []byte("RPUSH")
 	pExpireAtCmd = []byte("PEXPIREAT")
 )
 
@@ -38,14 +46,14 @@ func EntityToReply(key string, entity *database.DataEntity) *reply.MultiBulkStri
 	switch val := entity.Data.(type) {
 	case []byte:
 		cmd = stringToCmd(key, val)
-		//case List.List:
-		//	cmd = listToCmd(key, val)
-		//case *set.Set:
-		//	cmd = setToCmd(key, val)
-		//case dict.Dict:
-		//	cmd = hashToCmd(key, val)
-		//case *SortedSet.SortedSet:
-		//	cmd = zSetToCmd(key, val)
+	case List.List:
+		cmd = listToCmd(key, val)
+	case set.Set:
+		cmd = setToCmd(key, val)
+	case dict.Dict:
+		cmd = hashToCmd(key, val)
+	case *sortedset.SortedSet:
+		cmd = zSetToCmd(key, val)
 	}
 
 	if cmd == nil {
@@ -80,6 +88,64 @@ func stringToCmd(key string, bytes []byte) *reply.MultiBulkStringReply {
 	args[0] = setCmd
 	args[1] = []byte(key)
 	args[2] = bytes
+
+	return reply.MakeMultiBulkStringReply(args)
+}
+
+func zSetToCmd(key string, zSet *sortedset.SortedSet) *reply.MultiBulkStringReply {
+	args := make([][]byte, 2, 2+2*zSet.Len())
+	args[0] = zAddCmd
+	args[1] = []byte(key)
+
+	zSet.ForEach(0, zSet.Len(), false, func(element *sortedset.Element) bool {
+		args = append(args, []byte(strconv.FormatFloat(element.Score, 'f', -1, 64)))
+		args = append(args, []byte(element.Member))
+
+		return true
+	})
+
+	return reply.MakeMultiBulkStringReply(args)
+}
+
+func hashToCmd(key string, hash dict.Dict) *reply.MultiBulkStringReply {
+	args := make([][]byte, 2, 2+2*hash.Len())
+	args[0] = hSetCmd
+	args[1] = []byte(key)
+	hash.ForEach(func(key string, val interface{}) bool {
+		bytes, _ := val.([]byte)
+		args = append(args, []byte(key))
+		args = append(args, bytes)
+
+		return true
+	})
+
+	return reply.MakeMultiBulkStringReply(args)
+}
+
+func setToCmd(key string, set set.Set) *reply.MultiBulkStringReply {
+	args := make([][]byte, 2, 2+set.Len())
+	args[0] = sAddCmd
+	args[1] = []byte(key)
+	set.ForEach(func(member string) bool {
+		args = append(args, []byte(member))
+
+		return true
+	})
+
+	return reply.MakeMultiBulkStringReply(args)
+}
+
+func listToCmd(key string, list List.List) *reply.MultiBulkStringReply {
+	args := make([][]byte, 2, 2+list.Len())
+	args[0] = rPushCmd
+	args[1] = []byte(key)
+
+	list.ForEach(func(i int, v interface{}) bool {
+		bytes, _ := v.([]byte)
+		args = append(args, bytes)
+
+		return true
+	})
 
 	return reply.MakeMultiBulkStringReply(args)
 }
