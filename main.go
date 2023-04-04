bf32b482eb80313236381553cb959f5543e3eb4b
dawnzzz <635441876@qq.com>
2023-04-04 23:09:13 +0800

    add banner

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/dawnzzz/simple-redis/config"
 	"github.com/dawnzzz/simple-redis/logger"
 	"github.com/dawnzzz/simple-redis/redis/server"
@@ -12,9 +13,25 @@ import (
 var configFilename string
 var defaultConfigFileName = "config.yaml"
 
+const banner = `
+ ________   ___   _____ ______    ________   ___        _______    ________   _______    ________   ___   ________      
+|\   ____\ |\  \ |\   _ \  _   \ |\   __  \ |\  \      |\  ___ \  |\   __  \ |\  ___ \  |\   ___ \ |\  \ |\   ____\     
+\ \  \___|_\ \  \\ \  \\\__\ \  \\ \  \|\  \\ \  \     \ \   __/| \ \  \|\  \\ \   __/| \ \  \_|\ \\ \  \\ \  \___|_    
+ \ \_____  \\ \  \\ \  \\|__| \  \\ \   ____\\ \  \     \ \  \_|/__\ \   _  _\\ \  \_|/__\ \  \ \\ \\ \  \\ \_____  \   
+  \|____|\  \\ \  \\ \  \    \ \  \\ \  \___| \ \  \____ \ \  \_|\ \\ \  \\  \|\ \  \_|\ \\ \  \_\\ \\ \  \\|____|\  \  
+    ____\_\  \\ \__\\ \__\    \ \__\\ \__\     \ \_______\\ \_______\\ \__\\ _\ \ \_______\\ \_______\\ \__\ ____\_\  \ 
+   |\_________\\|__| \|__|     \|__| \|__|      \|_______| \|_______| \|__|\|__| \|_______| \|_______| \|__||\_________\
+   \|_________|                                                                                             \|_________|
+
+powered by https://github.com/dawnzzz/simple-redis
+
+`
+
 func main() {
 	flag.StringVar(&configFilename, "f", defaultConfigFileName, "the config file")
 	flag.Parse()
+
+	fmt.Print(banner)
 
 	// 加载配置文件
 	config.SetupConfig(configFilename)
