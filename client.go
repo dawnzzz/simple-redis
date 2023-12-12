d98883ed80e7ba401334554899d2d56aab9ab18a
dawnzzz <635441876@qq.com>
2023-12-12 15:21:48 +0800

    debug

--- client.go
+++ client.go
@@ -17,6 +17,7 @@ func main() {
 	flag.StringVar(&host, "h", "localhost", "the host of dawndis server")
 	flag.IntVar(&port, "p", 6179, "the port of dawndis server")
 	flag.IntVar(&keepalive, "k", 0, "the interval of server check client alive")
+	flag.Parse()
 
 	addr := fmt.Sprintf("%v:%v", host, port)
 
